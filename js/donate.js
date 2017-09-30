0f9aa4bc361092ff571e204660ecbcd06b86f78b
ELopez484 <elopez484@west-mec.org>
2017-09-29 18:12:10 -0700

    Final touches
    Add some CSS so that the div with the errorText id looks better,
    changed one of the names of the contact information and switch the
    place of a div with the errormessage in the HTML, and added more
    JavaScript to insure that the form is valid or not.

--- js/donate.js
+++ js/donate.js
@@ -25,19 +25,19 @@ function setUpPage() { // calls the functions that are within it
 }
 
 function validateAmount() {
-  var errorDiv = document.querySelectorAll("#donation .errorMessage")[0];
+  var errorDiv = document.querySelectorAll("#donation .errorMessage")[0]; // gets the CSS of the element with the donation id to be connected to the errorMessage class
   var donationMoney = document.getElementsByName("money"); // calls the elements that have the money name
   var fieldsetValidity = true; // Has the form be valid
-  var elementCount = donationMoney.length;
+  var elementCount = donationMoney.length; // counts the array of donationMoney
   try {
-    if (!donationMoney[0].checked && !donationMoney[1].checked && !donationMoney[2].checked && !donationMoney[3].checked && !donationMoney[4].checked) {
-      for (var i = 0; i < elementCount; i++) {
-        donationMoney[i].style.outline = "1px solid red";
+    if (!donationMoney[0].checked && !donationMoney[1].checked && !donationMoney[2].checked && !donationMoney[3].checked && !donationMoney[4].checked) { // if any of the radio buttons are not selected
+      for (var i = 0; i < elementCount; i++) { // loops through the array of
+        donationMoney[i].style.outline = "1px solid red"; // places a red outline
       }
-      fieldsetValidity = false;
+      fieldsetValidity = false; // makes form invalid
     } else {
-      for (var i = 0; i < elementCount; i++) {
-        donationMoney[i].style.outline = "";
+      for (var i = 0; i < elementCount; i++) { // loops through the array of elementCount
+        donationMoney[i].style.outline = ""; // does not have an outline
       }
     }
     if (fieldsetValidity === false) { // What will happen if the fieldsetValidity is set to false
@@ -54,9 +54,9 @@ function validateAmount() {
 }
 
 function validateOtherAmount() {
-  var errorDiv = document.querySelectorAll("#donation .errorMessage")[0];
-  var amountBox = document.getElementById("amount");
-  var fieldsetValidity = true;
+  var errorDiv = document.querySelectorAll("#donation .errorMessage")[0]; // gets the CSS of the element with the donation id to be connected to the errorMessage class
+  var amountBox = document.getElementById("amount"); // amountBox variable gets element that ahs the amount id
+  var fieldsetValidity = true; // makes the form valid
   try {
     if (document.getElementById("other").checked && amountBox.value === "") {
       fieldsetValidity = false; // form is invalid
@@ -77,17 +77,16 @@ function validateOtherAmount() {
     formValidity = false; // The form isn't valid
   }
 }
-// console.log(validateOtherAmount);
 
 function validateCardInfo() {
-  var errorDiv = document.querySelectorAll("#cardinfo .errorMessage")[0];
-  var fieldsetValidity = true;
-  var cardType = document.getElementsByName("CardType");
-  var cardNum = document.getElementById("ccNum");
-  var expiredNum = document.querySelectorAll("#cardinfo select");
-  var securityCode = document.getElementById("securcode");
-  var elementCount = expiredNum.length;
-  var currentElement;
+  var errorDiv = document.querySelectorAll("#cardinfo .errorMessage")[0];// gets the CSS of the element with the cardinfo id to be connected to the errorMessage class
+  var fieldsetValidity = true; // makes the form valid
+  var cardType = document.getElementsByName("CardType"); // cardType variable gets the elements that have the CardType name
+  var cardNum = document.getElementById("ccNum"); // cardNum variable gets the element that has ccNum id
+  var expiredNum = document.querySelectorAll("#cardinfo select"); // gets the CSS of the select elements that are within the element that has the cardinfo id
+  var securityCode = document.getElementById("securcode"); // securityCode variable gets the element that has the securcode id
+  var elementCount = expiredNum.length; // the elementCount counts the array of expiredNum
+  var currentElement; // the currentElement variable
   try {
     if (!cardType[0].checked && !cardType[1].checked && !cardType[2].checked && !cardType[3].checked) { // error no card checked
       for (var i = 0; i < cardType.length; i++) { // gets the array of cards
@@ -99,29 +98,26 @@ function validateCardInfo() {
         cardType[i].style.outline = ""; // Removes the outline
       }
     }
-    if (cardNum.value === "") {
-      cardNum.style.background = "rgb(255,0,0)";
-      fieldsetValidity = false
+    if (cardNum.value === "") { // the input of cardNum is empty
+      cardNum.style.background = "rgb(255,0,0)"; // changes the color of the background
+      fieldsetValidity = false // males the form invalid
+    } else {
+      cardNum.style.background = "white"; // leaves the background white
     }
-    else {
-      cardNum.style.background = "white";
-    }
-    for (var i = 0; i < elementCount; i++) {
-      currentElement = expiredNum[i];
-      if (currentElement.selectedIndex === -1) {
-        currentElement.style.border = "1px solid red";
-        fieldsetValidity = false;
-      }
-      else {
-        currentElement.style.border = "";
+    for (var i = 0; i < elementCount; i++) { // if none of the selects of currentElement are picked
+      currentElement = expiredNum[i]; // currentElement
+      if (currentElement.selectedIndex === -1) { // currentElement has the array of stateInfo
+        currentElement.style.border = "1px solid red"; // places a red outline
+        fieldsetValidity = false; // makes form invalid
+      } else {
+        currentElement.style.border = ""; // does not have an outline
       }
     }
-    if (securityCode.value === "") {
-      securityCode.style.background = "rgb(255,0,0)";
-      fieldsetValidity = false
-    }
-    else {
-      securityCode.style.background = "white";
+    if (securityCode.value === "") { // if the input of securityCode is empty
+      securityCode.style.background = "rgb(255,0,0)"; // places a red outline
+      fieldsetValidity = false // makes form invalid
+    } else {
+      securityCode.style.background = "white"; // does not have an outline
     }
     if (fieldsetValidity === false) { // What will happen if the fieldsetValidity isn't valid
       throw "Please insert all of your credit card information"; // throws in the error
@@ -137,15 +133,64 @@ function validateCardInfo() {
 }
 
 function validateContactInfo() {
-  var errorDiv = document.querySelectorAll("#contactinfo .errorMessage");
-  var fieldsetValidity = true;
-  var fullName = document.getElementsByName("name");
-  var address = document.getElementById("address");
-  var stateInfo = document.querySelectorAll("#contactinfo select");
-  var zipCode = document.getElementById("zipcode");
-  var agreeBox = document.getElementById("agree");
-  var elementCount = stateInfo.length;
-  var currentElement;
+  var errorDiv = document.querySelectorAll("#contactinfo .errorMessage")[0];// gets the CSS of the element with the contactinfo id to be connected to the errorMessage class
+  var fieldsetValidity = true; // makes the form valid
+  var fullName = document.getElementsByName("name"); // fullName variable gets the elements that have the name name
+  var address = document.getElementById("address"); // address variable gets the element that has the address id
+  var stateInfo = document.querySelectorAll("#contactinfo select"); // gets the CSS of the select that are within the element that has the contactinfo id
+  var zipCode = document.getElementById("zipcode"); // zipCode variable gets the element that has the zipcode id
+  var elementCount = stateInfo.length; // elementCount variable the array of stateInfo
+  var currentElement; // the currentElement variable
+
+  try {
+    if (fullName[0].value === "" || fullName[1].value === "") { // error no card checked
+      for (var i = 0; i < fullName.length; i++) { // gets the array of cards
+        fullName[i].style.background = "rgb(255,0,0)"; // places a outline around the unchecked checkboxes
+      }
+      fieldsetValidity = false; // sets the form invalid
+    } else {
+      for (var i = 0; i < fullName.length; i++) { // gets the array of cards
+        fullName[i].style.background = "white"; // Removes the outline
+      }
+    }
+    if (address.value === "") {
+      address.style.background = "rgb(255,0,0)"; // places a red outline
+      fieldsetValidity = false; // makes form invalid
+    } else {
+      address.style.background = "white"; // does not have an outline
+    }
+    for (var i = 0; i < elementCount; i++) {  // loops through the array of elementCount
+      currentElement = stateInfo[i]; // currentElement has the array of stateInfo
+      if (currentElement.selectedIndex === -1) { // if none of the selects of currentElement are picked
+        currentElement.style.border = "1px solid red"; // places a red outline
+        fieldsetValidity = false; // makes form invalid
+      } else {
+        currentElement.style.border = ""; // does not have an outline
+      }
+    }
+    if (zipCode.value === "") { //if input for zipCode is empty
+      zipCode.style.background = "rgb(255,0,0)"; // places a red outline
+      fieldsetValidity = false; // makes form invalid
+    } else {
+      zipCode.style.background = "white"; // does not have an outline
+    }
+    if (!document.getElementById("agree").checked) { // the checkbox that has the agree id is not checked
+      document.getElementById("agree").style.outline = "1px solid red"; // places a red outline
+      fieldsetValidity = false; // makes form invalid
+    } else {
+      document.getElementById("agree").style.outline = ""; // does not have an outline
+    }
+    if (fieldsetValidity === false) { // What will happen if the fieldsetValidity isn't valid
+      throw "Please insert all of your credit card information"; // throws in the error
+    } else { // What will happen if the fieldsetValidity is not false
+      errorDiv.style.display = "none"; // does not display the error
+      errorDiv.innerHTML = ""; // error is left blank
+    }
+  } catch (msg) {
+    errorDiv.style.display = "block"; // blocks the error
+    errorDiv.innerHTML = msg; // shows the error
+    formValidity = false; // The form isn't valid
+  }
 }
 
 function validateForm(evt) { // Depending if the whole form is filled, it will not allow the form to be submitted
@@ -156,10 +201,10 @@ function validateForm(evt) { // Depending if the whole form is filled, it will n
   }
   formValidity = true; // Makes the form valid
 
-  validateAmount();
-  validateOtherAmount();
-  validateCardInfo();
-  validateContactInfo();
+  validateAmount(); //the validateAmount function gets called
+  validateOtherAmount(); //the validateOtherAmount function gets called
+  validateCardInfo(); //the validateCardInfo function gets called
+  validateContactInfo(); //the validateContactInfo function gets called
 
   if (formValidity === true) { // form is valid
     document.getElementById("errorText").innerHTML = ""; // Won't display a message
